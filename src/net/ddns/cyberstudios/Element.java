90408cb4f81e3593fe851409cd53acd74322aaca
KGCybeX <k4gcybex@gmail.com>
2018-07-08 15:10:11 +0200

    Initial commit

---
+++ src/net/ddns/cyberstudios/Element.java
@@ -0,0 +1,161 @@
+package net.ddns.cyberstudios;
+
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Map;
+
+//TODO
+// add methods to check ID, hint, check question type, check type, etc
+
+public class Element {
+
+    public static Map.Entry<String, String> EmptyEntry = new Map.Entry<String, String>() {
+        @Override
+        public String getKey() {
+            return "";
+        }
+
+        @Override
+        public String getValue() {
+            return "";
+        }
+
+        @Override
+        public String setValue(String value) {
+            return "";
+        }
+    };
+
+    private Map<String, String> attributes = new HashMap<>();
+    private LinkedList<Element> children = new LinkedList<>();
+    private Element parent;
+    private String tag = "",
+            id = "",
+            hint = "",
+            type = "",
+            value = "";
+
+    public Element() {
+    }
+
+    public Element(String tag) {
+        this.tag = tag;
+    }
+
+    public Element(String tag, Element parent) {
+        this.tag = tag;
+        this.parent = parent;
+    }
+
+    public Element(String tag, Element parent, LinkedList<Element> children) {
+        this.tag = tag;
+        this.children = children;
+        this.parent = parent;
+    }
+
+    public Element(String tag, Element parent, LinkedList<Element> children, Map<String, String> attributes) {
+        this.tag = tag;
+        this.attributes = attributes;
+        this.children = children;
+        this.parent = parent;
+
+        processAttributes();
+    }
+
+    public Element(String tag, Element parent, Map<String, String> attributes) {
+        this.tag = tag;
+        this.attributes = attributes;
+        this.parent = parent;
+
+        processAttributes();
+    }
+
+    private void processAttributes() {
+        id = attributes.entrySet().stream().filter(s -> s.getValue().equals("id")).findFirst().orElse(EmptyEntry).getValue();
+        hint = attributes.entrySet().stream().filter(s -> s.getValue().equals("hint")).findFirst().orElse(EmptyEntry).getValue();
+        type = attributes.entrySet().stream().filter(s -> s.getValue().equals("type")).findFirst().orElse(EmptyEntry).getValue();
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+
+    public boolean hasParent(){
+        return parent!=null;
+    }
+
+    public boolean hasId(){
+        return !id.isEmpty();
+    }
+
+    public boolean hasHint(){
+        return !hint.isEmpty();
+    }
+
+    public String getHint() {
+        return hint;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public String getTag() {
+        return tag;
+    }
+
+    public void setTag(String tag) {
+        this.tag = tag;
+    }
+
+    public Map<String, String> getAttributes() {
+        return attributes;
+    }
+
+    public void addAttribute(String attrKey, String attrValue) {
+        this.attributes.put(attrKey, attrValue);
+    }
+
+    public void addChild(Element child) {
+        this.children.add(child);
+    }
+
+    public LinkedList<Element> getChildren() {
+        return children;
+    }
+
+    public Element getParent() {
+        return parent;
+    }
+
+    public void setParent(Element parent) {
+        this.parent = parent;
+    }
+
+    public String getPreTag(){
+        String att = attributes.entrySet().stream().map(attrEntry -> " " + attrEntry.getKey() + "=" + attrEntry.getValue()).reduce(String::concat).orElse("");
+        return "<" + tag + att + ">";
+    }
+
+    public String getPostTag(){
+        return "</" + tag + ">";
+    }
+
+    public String getChildrenContent(){
+        return children.stream().map(Element::toString).reduce(String::concat).orElse("");
+    }
+
+    @Override
+    public String toString() {
+        //TODO Check this
+        return getPreTag() + getChildrenContent() + getPostTag();
+    }
+}
---
+++ src/net/ddns/cyberstudios/XMLTools.java
@@ -0,0 +1,138 @@
+package net.ddns.cyberstudios;
+
+import org.w3c.dom.Attr;
+import org.w3c.dom.Document;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+import java.io.File;
+import java.io.IOException;
+import java.io.StringWriter;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Map;
+
+public class XMLTools {
+    /**
+     * Text representation of document
+     *
+     * @param newDoc
+     * @return
+     */
+    public static String toString(Document newDoc) {
+        try {
+            DOMSource domSource = new DOMSource(newDoc);
+            Transformer transformer = null;
+            transformer = TransformerFactory.newInstance().newTransformer();
+            StringWriter sw = new StringWriter();
+            StreamResult sr = new StreamResult(sw);
+            transformer.transform(domSource, sr);
+            return sw.toString();
+        } catch (TransformerException e) {
+            e.printStackTrace();
+        }
+        return "<root/>";
+    }
+
+    /**
+     * Reads file, parses document and constructs a readable and managable {@link Element} hierachy to easily traverse
+     *
+     * @param xml_file
+     * @return
+     */
+    public static Element parseDocument(File xml_file) {
+        try {
+            DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+            Document doc = dBuilder.parse(xml_file);
+            return parseDocument(doc);
+        } catch (ParserConfigurationException | SAXException | IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    /**
+     * Reads document and constructs a readable and managable {@link Element} hierachy to easily traverse
+     *
+     * @param xml_document
+     * @return
+     */
+    public static Element parseDocument(Document xml_document) {
+        return parse(xml_document.getDocumentElement(), null);
+    }
+
+    public static Element parse(org.w3c.dom.Element root, Element parent) {
+        Element element = new Element(root.getTagName(), parent);
+        for (int i = 0; i < root.getAttributes().getLength(); i++) {
+            Attr item = (Attr) root.getAttributes().item(i);
+            String attrKey = item.getNodeName();
+            String attrValue = item.getNodeValue();
+            element.addAttribute(attrKey, attrValue);
+        }
+        NodeList childNodes = root.getChildNodes();
+        for (int i = 0; i < childNodes.getLength(); i++) {
+            Node item = childNodes.item(i);
+            if (item.getNodeType() == Node.ELEMENT_NODE) {
+                org.w3c.dom.Element e = (org.w3c.dom.Element) item;
+                String tag = e.getTagName();
+                Element childNode = parse(e, element);
+                if (tag.equals("component")) {
+                    if (item.getFirstChild() != null && item.getFirstChild().getNodeType() == Node.TEXT_NODE) {
+                        childNode.setValue(item.getFirstChild().getNodeValue());
+                    }
+                }
+                element.addChild(childNode);
+            }
+        }
+        return element;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
